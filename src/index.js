55c6ac8d716d6b2afc933df2369873cdbec06a2b
HannahSmirnova <alexamy1986@gmail.com>
2025-08-22 14:39:01 -0600

    changed instruction box name

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert("Hi there!");
+function generatePractice(event) {
+  event.preventDefault();
+  alert("Generating Practice");
+}
+let practiceFormElement = document.querySelector("#practice");
+practiceFromElement.addEventListener("submit", generatePractice);
